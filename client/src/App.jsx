5c991f170f106e53a8643fc827bdd30ac112e5df
Srigana Pulikantham <srigana2003@gmail.com>
2025-06-13 20:51:12 +0530

    frontend done

---
+++ client/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="bg-red-500 p-4 text-white">
+      <h1 className="text-3xl font-bold">Hello World</h1>
+      <p>Your content here</p>
+    </div>
+  )
+}
